fff6589d729b6add411fc64bffac5ad32299df87
Stefano <ste.morelli1@stud.uniroma3.it>
2025-05-10 17:11:19 +0200

    Aggiunti placholder per le classi Repository

---
+++ src/main/java/it/uniroma3/siwbooks/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package it.uniroma3.siwbooks.repository;
+
+import it.uniroma3.siwbooks.model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Integer> {
+}
