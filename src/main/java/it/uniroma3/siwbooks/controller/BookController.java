defcc9b024aece1b9bdf7a5a2688a01cf1f2a370
Stefano <ste.morelli1@stud.uniroma3.it>
2025-05-11 20:17:24 +0200

    Aggiunto prototipo di pagina per visualizzare l'elenco dei libri

--- src/main/java/it/uniroma3/siwbooks/controller/BookController.java
+++ src/main/java/it/uniroma3/siwbooks/controller/BookController.java
@@ -1,7 +1,18 @@
 package it.uniroma3.siwbooks.controller;
 
+import it.uniroma3.siwbooks.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
+    @Autowired
+    private BookService bookService;
+    @GetMapping("/book")
+    public String showBooks(Model model) {
+        model.addAttribute("books",bookService.getAllBooks());
+        return "books";
+    }
 }
--- src/main/java/it/uniroma3/siwbooks/service/BookService.java
+++ src/main/java/it/uniroma3/siwbooks/service/BookService.java
@@ -1,11 +1,17 @@
 package it.uniroma3.siwbooks.service;
 
+import it.uniroma3.siwbooks.model.Book;
 import it.uniroma3.siwbooks.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
+    public List<Book> getAllBooks() {
+        return (List<Book>) bookRepository.findAll();
+    }
 }
