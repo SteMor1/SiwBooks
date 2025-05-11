5b76138cecb5509f9c1c8877bfe85ee41b13af06
Stefano <ste.morelli1@stud.uniroma3.it>
2025-05-11 19:52:16 +0200

    Aggiunta hompage semplice

--- src/main/java/it/uniroma3/siwbooks/controller/MainController.java
+++ src/main/java/it/uniroma3/siwbooks/controller/MainController.java
@@ -1,7 +1,13 @@
 package it.uniroma3.siwbooks.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
 }
