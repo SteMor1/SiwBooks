b09e359ef5f37cb3990e9fbbeef9b3afec2ea586
Stefano <ste.morelli1@stud.uniroma3.it>
2025-05-18 12:46:18 +0200

    Piccoli aggiustamenti a User

--- src/main/java/it/uniroma3/siwbooks/service/UserService.java
+++ src/main/java/it/uniroma3/siwbooks/service/UserService.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siwbooks.service;
 
+import it.uniroma3.siwbooks.model.User;
 import it.uniroma3.siwbooks.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +9,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
 }
