038909741258d0dec9eb66d04bda7d673ef9dd08
Stefano <ste.morelli1@stud.uniroma3.it>
2025-06-08 16:13:53 +0200

    ++Adesso ReviewRepository offre un metodo per ottenere tutte le recensioni di un libro

--- src/main/java/it/uniroma3/siwbooks/repository/ReviewRepository.java
+++ src/main/java/it/uniroma3/siwbooks/repository/ReviewRepository.java
@@ -3,6 +3,10 @@ package it.uniroma3.siwbooks.repository;
 import it.uniroma3.siwbooks.model.Review;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends CrudRepository<Review, Long> {
     public boolean existsByBookIdAndAuthorId(Long bookId, Long authorId);
+
+    List<Review> findByBookId(Long bookId);
 }
