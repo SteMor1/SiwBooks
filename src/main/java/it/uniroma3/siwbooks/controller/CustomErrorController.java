cafd0156f8a82beda0d8d72823cf2d0480644cf0
Stefano <ste.morelli1@stud.uniroma3.it>
2025-06-26 08:32:55 +0200

    Aggiunte schermate d'errore

---
+++ src/main/java/it/uniroma3/siwbooks/controller/CustomErrorController.java
@@ -0,0 +1,25 @@
+package it.uniroma3.siwbooks.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute("jakarta.servlet.error.status_code");
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.FORBIDDEN.value()) {
+                return "error/forbidden";
+            }
+        }
+        return "error/generic";
+    }
+}
